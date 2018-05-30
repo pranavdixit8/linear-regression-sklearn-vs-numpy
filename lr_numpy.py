4e4d030e33f07c5c72705555cbe7e3b0ec256ea4
pranav <pranavdixit08@gmail.com>
2018-05-29 20:49:50 -0700

    add compute_squared_mean_error function to calculate error

--- lr_numpy.py
+++ lr_numpy.py
@@ -4,6 +4,26 @@ import matplotlib.pyplot as plt
 
 
 
+
+
+def compute_squared_mean_error(b, m , bmi_life_data):
+
+	total_error = 0
+
+	Y = bmi_life_data[['Life expectancy']]
+	N = float(len(Y))
+
+	for i in range(0, len(Y)):
+
+		x = bmi_life_data.loc[i, 'BMI']
+		y = bmi_life_data.loc[i,'Life expectancy']
+
+		total_error+=(y - (m*x + b))**2 
+
+	return total_error/N
+
+
+
 def step_gradient(current_m, current_b, bmi_life_data, learning_rate):
 
 	b_gradient = 0
@@ -51,7 +71,13 @@ def lr_numpy():
 	num_iteration  =  2000
 	learning_rate = 0.0001
 
+	print("Running gradient Descent:")
+
+	print("Starting  with b = {}, m = {}, squared mean error = {}".format(initial_b, initial_m, compute_squared_mean_error(initial_b, initial_m, bmi_life_data)))
+
 	[m,b] =  gradient_descent(bmi_life_data, initial_m, initial_b, learning_rate, num_iteration)
+
+	print("After running  {} iterations,  b = {}, m = {}, squared mean error = {}".format(num_iteration, b, m, compute_squared_mean_error(b, m, bmi_life_data)))
 	
 	print(m)
 	print(b)
